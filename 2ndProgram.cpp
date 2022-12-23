53ca246a966ab3f7829c0361c4817d30c8af3dbd
sachin77999 <sachinsoni77999@gmail.com>
2022-12-23 12:21:23 +0530

    updates

---
+++ 2ndProgram.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout << "Enter the value of a" << endl;
+    cin >> a;
+    cout << "Enter the value of b" << endl;
+    cin >> b;
+    if(a > b)
+    {
+        cout << "A is greater " << endl;
+    }
+    if(b > a)
+    {
+        cout << "B is greator " << endl;
+    }
+
+    return 0;
+}
