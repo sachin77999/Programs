0bcd913e969576a6de46b8e2551a9245844882b6
sachin77999 <sachinsoni77999@gmail.com>
2022-12-23 12:05:50 +0530

    updates

---
+++ conditionalsAndLoops.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int a 
+  //  cin >> n;
+  //  cout << "Hello" << endl;
+  //  cout << "value of n is : " << n << endl;
+  cin >> a;
+    if(a > 0)
+    {
+        cout << "A is positive " << endl;
+    }
+}
