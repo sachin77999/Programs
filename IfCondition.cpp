ec911fbb179e3e70ee281ed62707a6f6263137c4
sachin77999 <sachinsoni77999@gmail.com>
2022-12-23 12:08:46 +0530

    updates

---
+++ IfCondition.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+    int a;
+    cin >> a;
+    if(a > 0)
+    {
+        cout << "A is positive" << endl;
+    }
+    else {
+        cout << "A is not positive" << endl;
+    }
+
+    return 0;
+}
