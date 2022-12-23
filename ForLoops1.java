d213a2f572cf38c4094a5cf47fe6f83bfad468e5
sachin77999 <sachinsoni77999@gmail.com>
2022-12-23 13:55:35 +0530

    updates

--- ForLoops1.java
+++ ForLoops1.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-public class ForLoops1 {
+public class ForLoops1 
+{
 
     public static void main(String args[])
     {
@@ -10,4 +11,4 @@ public class ForLoops1 {
     }
 
     }
-}
+
